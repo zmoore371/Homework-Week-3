a7130cd154f9e499995056685930bd9dd7c72cfd
Zmoore <zmoore371@gmail.com>
2021-08-20 10:19:40 -0400

    will write to password box. error message will pop up if count is less than 7 but will still write a password. need to prevent that from happening

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,7 @@ const randomFunction = {
   symbol: randomSymbol,
 };
 
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
+
 
 // Write password to the #password input
 function writePassword() {
@@ -17,7 +16,7 @@ function writePassword() {
   var getLowercase = window.confirm("Would you like your password to have lower case letters? y/n");
   var getUppercase = window.confirm("Would you like your password to have upper case letters? y/n");
   var getNumbers = window.confirm("Would you like your password to have numbers? y/n");
-  var getSpecialCharacters = window.confirm("Would you like your password to have special characters? (! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { } | ` ) y/n");
+  var getSymbols = window.confirm("Would you like your password to have special characters / symbols ? (! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { } | ` ) y/n");
   var length = Number(getCharacters);
   
 
@@ -30,18 +29,11 @@ function writePassword() {
     return getCharacters
   }
   }  
-  
-  
 
-  
-
-  
  numCharacters();    
- let answers = [length, getLowercase, getUppercase, getNumbers, getSpecialCharacters];
 
- console.log(answers);
-
- password = generatePassword(length, getLowercase, getUppercase, getNumbers, getSpecialCharacters);
+ password = generatePassword(length, getLowercase, getUppercase, getNumbers, getSymbols);
+ return password;
 
 }
 
@@ -62,28 +54,27 @@ function generatePassword(length, lower, upper, number, symbol) {
 		typesArray.forEach(type => {
 			const funcName = Object.keys(type)[0];
 
-      console.log(funcName);
+      // console.log(funcName);
 			generatedPassword += randomFunction[funcName]();
 		});
 
 	}
+
   const password = generatedPassword.slice(0, length);
-	console.log(password);
-	return password;
+	// console.log(password);
+  passwordText.value = password;
+	return passwordText.value;
   
 }
 
-
+// console.log(password);
 //write to password area
-
-
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 
 // Add event listener to generate button
 // this is looking for a button click and calls the writePassword function
 generateBtn.addEventListener("click", writePassword);
-
-
-
 
 function randomLower() { 
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97 )
